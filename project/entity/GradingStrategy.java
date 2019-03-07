ba5707c77e3c006c4bb69d359f2927a9d713baa7
KN1fe123 <31401472+KN1fe123@users.noreply.github.com>
2019-03-06 23:29:03 -0800

    Add files via upload

---
+++ project/entity/GradingStrategy.java
@@ -0,0 +1,14 @@
+package entity;
+
+import java.util.List;
+
+import entity.Grade;
+import entity.SizeException;
+
+public interface GradingStrategy {
+	
+	static Grade calculate(String key, List<Grade> grades) throws SizeException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
---
+++ project/entity/TotalStrategy.java
@@ -0,0 +1,36 @@
+package entity;
+import java.util.List;
+import entity.Grade;
+
+/*
+ * Author: Abhishek Chandak,Rishiraj Bhosale
+ * */
+
+public class TotalStrategy implements GradingStrategy  {
+	double total = 0.0;
+	Grade grade;
+	
+	public TotalStrategy(){
+		
+	}
+
+	public Grade calculate(String key, List<Grade> grades) throws SizeException {
+		// TODO Auto-generated method stub
+		double number = 0.0;
+		
+		if(grades.equals(null) || grades.size()==0)
+			throw new SizeException("List cant be empty");
+		else {
+				for (int i = 0; i < grades.size(); i++) {
+					
+					number = Missing.doubleValue(grades.get(i).getValaue());
+						
+					total += number;
+				}
+			grade =	new Grade(key, total);		
+		}
+		return grade;
+
+		}
+	}
+
